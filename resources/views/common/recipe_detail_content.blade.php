80eb4b4042d9ac7efd8e064cccefc79ad90d478c
duc <duclh711@gmail.com>
2017-12-27 00:32:09 +0700

    Update view detail

--- resources/views/common/recipe_detail_content.blade.php
+++ resources/views/common/recipe_detail_content.blade.php
@@ -13,7 +13,7 @@
                         Giá nguyên liệu: <span class="product-amount">{{number_format($recipe->price, 0, ',', '.')}} VNĐ</span>
                     </div>
                     <div class="form-single-product">
-                        <button type="button" class="single-add-to-cart-button"><i class="icon_cart_alt"></i> Mua ngay</button>
+                        <button type="button" class="single-add-to-cart-button" data-toggle="modal" data-target="#modal-buy"><i class="icon_cart_alt"></i> Mua ngay</button>
                     </div>
                 </div>
             </div>
@@ -41,8 +41,53 @@
         <div class="col-md-5">
             <h3>Video hướng dẫn</h3>
             <div class="video_embed post-img">
-                <iframe height="500" src="{{$recipe->video}}"></iframe>
+                <iframe width="560" height="315" src="{{$recipe->video}}" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
             </div>
         </div>
     </div>
 </div><!-- End container -->
+
+
+  <div class="modal fade" id="modal-buy">
+    <div class="modal-dialog">
+      <form method='post' action='' enctype="multipart/form-data">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title">Đặt hàng: </h4>
+        </div>
+        <div class="modal-body">
+            <div class="form-login">
+          <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="name">Tên</label>
+                  <input type="text" class="form-control" id="name" placeholder="Tên" required="">
+                </div>
+                <div class="form-group">
+                  <label for="address">Địa chỉ</label>
+                  <input type="text" class="form-control" id="address" placeholder="Địa chỉ" required="">
+                </div>
+                <div class="form-group">
+                  <label for="phone">Số điện thoại</label>
+                  <input type="text" class="form-control" id="phone" placeholder="Số điện thoại" required="">
+                </div>
+
+              </div>
+              <!-- /.box-body -->
+            </form>
+        </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Đóng</button>
+          <input type="button" class="btn btn-primary" id="upload" value="Đặt hàng"></input>
+        </div>
+      </div>
+      </form>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+  <!-- /.modal -->
+</section>
