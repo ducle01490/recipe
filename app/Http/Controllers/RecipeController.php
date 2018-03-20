3a3f146757211c473264344ee5421919c7a6f05a
duc <duclh711@gmail.com>
2018-03-20 22:48:01 +0700

    Add cache to DB query

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 use App\Recipe;
 use App\Compilation;
@@ -32,18 +33,26 @@ class RecipeController extends Controller
      */
     public function index(Request $request)
     {
+        $page = $request->get('page')?:1;
+        $keyCache = 'recipe_page_'.$page;
+        $minutes = 1;
+
+
         $compilation = DB::table('compilations')->where('status', 1)->select('id', 'title', 'thumb', 'video', 'status', DB::raw("'compilation' as type"), 'updated_at');
 
-        $recipe = DB::table('recipes')->where('status', 1)->whereNull('compilationId')->select('id', 'title', 'thumb', 'video', 'status', DB::raw("'recipe' as type"), 'updated_at')->union($compilation);;
+        $recipe = DB::table('recipes')->where('status', 1)->whereNull('compilationId')->select('id', 'title', 'thumb', 'video', 'status', DB::raw("'recipe' as type"), 'updated_at')->union($compilation);
 
-        $page = $request->get('page')?:1;
+        
         $paginator = new Paginator();
+        $paginator->setQuery($recipe)->setCurrentPage($page)->setPerPage(3);
 
-        $recipes = $paginator->setQuery($recipe)->setCurrentPage($page)->setPerPage(12)->getData();
-
+        $recipes = Cache::remember($keyCache, $minutes, function () use ($paginator) {
+            return $paginator->getData();
+        });
 
         return view('recipes.index', compact('recipes', 'paginator'));
     }
+
 
     public function recipe(Request $request, $recipe_id)
     {
@@ -55,7 +64,13 @@ class RecipeController extends Controller
         $arrId = explode('-',$recipe_id);
         $recipe_id = $arrId[count($arrId) - 1];
 
-        $recipe = Recipe::where('id', $recipe_id)->where('status', 1)->get()->first();
+        $keyCache = 'recipe_'.$recipe_id;
+        $minutes = 1;
+
+        $recipe = Cache::remember($keyCache, $minutes, function () use ($recipe_id) {
+            return Recipe::where('id', $recipe_id)->where('status', 1)->get()->first();
+        });
+
         if (is_null($recipe)) {
             abort(404);
         }
@@ -65,11 +80,17 @@ class RecipeController extends Controller
         $siteImage = $recipe->thumb;
 
         if ($recipe->compilationId) {
-            $compilation = Compilation::find($recipe->compilationId);
+            $compilationId = $recipe->compilationId;
+            $keyCacheCompilation = 'compilation_'.$compilationId;
+            $compilation = Cache::remember($keyCacheCompilation, $minutes, function () use ($compilationId) {
+                return Compilation::find($compilationId);
+            });
+
             return view('recipes.detail', compact('recipe', 'siteTitle', 'siteDescription', 'siteImage', 'compilation'));
         } else {
             return view('recipes.detail', compact('recipe', 'siteTitle', 'siteDescription', 'siteImage'));
         }
+        
     }
 
 
@@ -82,13 +103,22 @@ class RecipeController extends Controller
 
         $arrId = explode('-',$compilation_id);
         $compilation_id = $arrId[count($arrId) - 1];
+        $minutes = 1;
 
-        $compilation = Compilation::where('id', $compilation_id)->where('status', 1)->get()->first();
+        $keyCacheCompilation = 'compilation_'.$compilation_id;
+        $compilation = Cache::remember($keyCacheCompilation, $minutes, function () use ($compilation_id) {
+            return Compilation::where('id', $compilation_id)->where('status', 1)->get()->first();
+        });
+
         if (is_null($compilation)) {
             abort(404);
         }
 
-        $recipes = Recipe::where('compilationId', $compilation_id)->where('status', 1)->get();
+        $keyCacheCompilationRecipe = 'recipes_compilation_'.$compilation_id;
+
+        $recipes = Cache::remember($keyCacheCompilationRecipe, $minutes, function () use ($compilation_id) {
+            return Recipe::where('compilationId', $compilation_id)->where('status', 1)->get();
+        });
 
         $siteTitle = $compilation->title;
         $siteDescription = 'Click để xem các món ăn hấp dẫn!';
